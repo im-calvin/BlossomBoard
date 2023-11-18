c6ae802b27f34143b4cf9e122738236ff0303b5d
im-calvin <kelvinwong0519@gmail.com>
2023-11-18 03:15:25 -0800

    sockets

---
+++ frontend/next.config.mjs
@@ -0,0 +1,47 @@
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+await import("./src/env.mjs");
+
+/** @type {import("next").NextConfig} */
+const config = {
+  // eslint-disable-next-line @typescript-eslint/require-await
+  headers: async () => {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "http://localhost:8080",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
+  reactStrictMode: true,
+
+  /**
+   * If you are using `appDir` then you must comment the below `i18n` config out.
+   *
+   * @see https://github.com/vercel/next.js/issues/41980
+   */
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+};
+
+export default config;
---
+++ frontend/src/utils/readEnv.ts
@@ -0,0 +1,6 @@
+export const readEnv = (env: string): string => {
+  const realEnv = process.env[env];
+  if (realEnv !== undefined) return realEnv;
+
+  throw new Error(`${env} was not found.`);
+};
